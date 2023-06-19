14add26d2968b7dddac2046376aacb31e59786c4
Icchi16 <duynghiemhn16@gmail.com>
2023-06-19 16:58:25 +0700

    add mongo db

--- app/(site)/components/authForm.tsx
+++ app/(site)/components/authForm.tsx
@@ -1,7 +1,11 @@
 "use client";
-import Input from "@/app/components/input/input"
+import Button from "@/app/components/Button";
+import Input from "@/app/components/input/input";
 import { useCallback, useState } from "react";
 import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
+import AuthSocialButton from "./authSocialButton";
+
+import { BsGithub, BsGoogle, BsFacebook } from "react-icons/bs";
 
 type Variant = "LOGIN" | "REGISTER";
 
@@ -39,18 +43,77 @@ const AuthForm = () => {
     if (variant === "LOGIN") {
       // NextAuth SignIn
     }
-
-    return (
-      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-        <div className="bg-white px-4 py-8 shadow sm:rounded-lg s,:px-10"></div>
-        <form
-          className="space4--y-6"
-          onSubmit={handleSubmit(onSubmit)}
-        >
-          <Input />
-        </form>
-      </div>
-    );
   };
+
+  return (
+    <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+      <div className="bg-white px-4 py-8 shadow sm:rounded-lg s,:px-10">
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          {variant === "REGISTER" && (
+            <Input
+              id="name"
+              label="Name"
+              register={register}
+              errors={errors}
+              disabled={isLoading }
+            />
+          )}
+          <Input
+            id="email"
+            label="Email address"
+            type="email"
+            register={register}
+            errors={errors}
+            disabled={isLoading}
+          />
+          <Input
+            id="password"
+            label="Password"
+            type="password"
+            register={register}
+            errors={errors}
+            disabled={isLoading}
+          />
+          <Button disabled={isLoading} fullWidth type="submit">
+            {variant === "LOGIN" ? "Sign in" : "Register"}
+          </Button>
+        </form>
+
+        <div className="mt-6">
+          <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+              <div className="w-full border-t border-gray-300" />
+            </div>
+            <div className="relative flex justify-center text-sm">
+              <span className="bg-white px-2 text-gray-500">
+                Or continue with
+              </span>
+            </div>
+          </div>
+
+          <div className="mt-6 flex gap-2">
+            <AuthSocialButton
+              icon={BsGithub}
+              onClick={() => socialAction("github")}
+            />
+            <AuthSocialButton
+              icon={BsGoogle}
+              onClick={() => socialAction("google")}
+            />
+          </div>
+        </div>
+
+        <div className="flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500">
+          {variant == "LOGIN"
+            ? "New to Messenger ?"
+            : "Already have an account ?"}
+          <div onClick={toggleVariant} className="underline cursor-pointer">
+            {variant === "LOGIN" ? "Create an account" : "Login"}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
+
 export default AuthForm;
--- app/components/input/input.tsx
+++ app/components/input/input.tsx
@@ -2,7 +2,7 @@
 
 import clsx from "clsx";
 
-import { FieldError, FieldValues, UseFormRegister } from "react-hook-form";
+import { FieldErrors, FieldValues, UseFormRegister } from "react-hook-form";
 
 interface InputProps {
   label: string;
@@ -10,7 +10,7 @@ interface InputProps {
   type?: string;
   required?: boolean;
   register: UseFormRegister<FieldValues>;
-  errors: FieldError;
+  errors: FieldErrors;
   disabled?: boolean;
 }
 
@@ -31,6 +31,35 @@ const Input: React.FC<InputProps> = ({
       >
         {label}
       </label>
+      <div className="mt-2">
+        <input
+          id={id}
+          type={type}
+          autoComplete={id}
+          disabled={disabled}
+          {...register(id, { required })}
+          className={clsx(
+            `
+          form-input
+          block
+          w-full
+          rounded-md
+          border-0
+          text-gray-900
+          shadow-sm
+          ring-1
+          ring-inset
+          ring-gray-300
+          focus:ring-2
+          focus:ring-inset
+          focus:ring-sky-600
+          sm:text-sm
+          sm:leading-6`,
+            errors[id] && "focus:ring-rose-500",
+            disabled && "opacity-50 cursor-default"
+          )}
+        />
+      </div>
     </div>
   );
 };
