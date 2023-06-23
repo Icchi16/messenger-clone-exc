7411cf7a7803de3dc68611cf7af0ac2b82e2b77e
Icchi16 <duynghiemhn16@gmail.com>
2023-06-23 17:11:01 +0700

    implement auth for login

--- app/(site)/components/authForm.tsx
+++ app/(site)/components/authForm.tsx
@@ -2,11 +2,14 @@
 import Button from "@/app/components/button";
 import Input from "@/app/components/input/input";
 import { useCallback, useState } from "react";
-import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
+import { useForm, FieldValues, SubmitHandler, set } from "react-hook-form";
 import AuthSocialButton from "./authSocialButton";
 
-import { BsGithub, BsGoogle, BsFacebook } from "react-icons/bs";
+import { BsGithub, BsGoogle } from "react-icons/bs";
 import axios from "axios";
+
+import { toast } from "react-hot-toast";
+import { signIn } from "next-auth/react";
 
 type Variant = "LOGIN" | "REGISTER";
 
@@ -38,12 +41,43 @@ const AuthForm = () => {
     setIsLoading(true);
 
     if (variant === "REGISTER") {
-      axios.post("/api/register", data);
+      axios
+        .post("/api/register", data)
+        .catch(() => toast.error("Something went wrong"))
+        .finally(() => setIsLoading(false));
     }
 
     if (variant === "LOGIN") {
-      // NextAuth SignIn
+      signIn("credentials", { ...data, redirect: false })
+        .then((callback) => {
+          if (callback?.error) {
+            toast.error("Invalid credentials");
+          }
+
+          if (callback?.ok && !callback?.error) {
+            toast.success("Logged in");
+          }
+        })
+        .finally(() => setIsLoading(false));
     }
+  };
+
+  const socialAction = (action: string) => {
+    setIsLoading(true);
+
+    signIn(action, { redirect: false })
+      .then((callback) => {
+        if (callback?.error) {
+          toast.error("Invalid credentials");
+        }
+
+        if (callback?.ok && !callback?.error) {
+          toast.success("Logged in!");
+        }
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   };
 
   return (
