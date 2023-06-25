c6fcf3bc05353f0a536c201c4958ead1875fcc13
SirIcchi <duynghiemhn16@gmail.com>
2023-06-25 09:57:07 +0700

    working on UserBar

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,10 +1,16 @@
 import Sidebar from "../components/sideBar/sidebar";
+import getUsers from "../actions/getUsers";
 
-const UsersLayout = ({ children }: { children: React.ReactNode }) => {
+const UsersLayout = async ({ children }: { children: React.ReactNode }) => {
+  const users = await getUsers();
+
   return (
     // @ts-expect-error Server Component
     <Sidebar>
-      <div className="h-full">{children}</div>
+      <div className="h-full">
+        <UserList items={users} />
+        {children}
+      </div>
     </Sidebar>
   );
 };
